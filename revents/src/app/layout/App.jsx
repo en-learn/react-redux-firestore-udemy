91049e7a6e082e44a2a66a4ea3f16227f2220af9
Elias Norrby <elias.norrby@gmail.com>
2019-10-06 10:35:23 +0200

    remove absolute imports
    It breaks go to definition :(

--- revents/src/app/layout/App.jsx
+++ revents/src/app/layout/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import EventDashboard from "features/events/EventDashboard/EventDashboard";
-import NavBar from "features/nav/NavBar/NavBar";
+import EventDashboard from "../../features/events/EventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar/NavBar";
 import { Container } from "semantic-ui-react";
 
 class App extends Component {
