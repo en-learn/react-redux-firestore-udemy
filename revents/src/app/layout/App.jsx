90f3ac7c5e80605755ff23a109d87dacd002a1fc
Elias Norrby <elias.norrby@gmail.com>
2019-10-08 13:21:58 +0200

    L44: Routing configuration

--- revents/src/app/layout/App.jsx
+++ revents/src/app/layout/App.jsx
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 import EventDashboard from "../../features/events/EventDashboard/EventDashboard";
 import NavBar from "../../features/nav/NavBar/NavBar";
 import { Container } from "semantic-ui-react";
+import { Route } from "react-router-dom";
+import HomePage from "../../features/home/HomePage";
+import EventDetailedPage from "../../features/events/EventDetailed/EventDetailedPage";
+import PeopleDashboard from "../../features/user/PeopleDashboard/PeopleDashboard";
+import UserDetailedPage from "../../features/user/UserDetailed/UserDetailedPage";
+import SettingsDashboard from "../../features/user/Settings/SettingsDashboard";
+import EventsForm from "../../features/events/EventForm/EventForm";
 
 class App extends Component {
   render() {
@@ -9,7 +16,13 @@ class App extends Component {
       <>
         <NavBar />
         <Container className="main">
-          <EventDashboard />
+          <Route path="/" exact component={HomePage} />
+          <Route path="/events" component={EventDashboard} />
+          <Route path="/events/:id" component={EventDetailedPage} />
+          <Route path="/people" component={PeopleDashboard} />
+          <Route path="/people/:id" component={UserDetailedPage} />
+          <Route path="/settings" component={SettingsDashboard} />
+          <Route path="/createEvent" component={EventsForm} />
         </Container>
       </>
     );
