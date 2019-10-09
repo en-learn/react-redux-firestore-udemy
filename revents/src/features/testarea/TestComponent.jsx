9bfafaa31cfd47d3d24dfb647a34b1a09afbc597
Elias Norrby <elias.norrby@gmail.com>
2019-10-09 09:47:00 +0200

    L55: Creating our first actions

--- revents/src/features/testarea/TestComponent.jsx
+++ revents/src/features/testarea/TestComponent.jsx
@@ -1,17 +1,30 @@
 import React from "react";
 import { connect } from "react-redux";
+import { incrementCounter, decrementCounter } from "./testActions";
+import { Button } from "semantic-ui-react";
 
 const mapStateToProps = state => ({
   data: state.data,
 });
 
+const mapDispatchToProps = {
+  incrementCounter,
+  decrementCounter,
+};
+
 const TestComponent = props => {
+  const { data, incrementCounter, decrementCounter } = props;
   return (
     <div>
       <h1>Test Component</h1>
       <h3>The answer is: {props.data}</h3>
+      <Button onClick={incrementCounter} positive content="Increment" />
+      <Button onClick={decrementCounter} negative content="Decrement" />
     </div>
   );
 };
 
-export default connect(mapStateToProps)(TestComponent);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TestComponent);
