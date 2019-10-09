fc2e7e47bfcc385210eb21e72325ab9a7f191ddf
Elias Norrby <elias.norrby@gmail.com>
2019-10-09 18:02:54 +0200

    L66: Adding the Event detailed page header component

--- revents/src/features/testarea/TestComponent.jsx
+++ revents/src/features/testarea/TestComponent.jsx
@@ -17,7 +17,7 @@ const TestComponent = props => {
   return (
     <div>
       <h1>Test Component</h1>
-      <h3>The answer is: {props.data}</h3>
+      <h3>The answer is: {data}</h3>
       <Button onClick={incrementCounter} positive content="Increment" />
       <Button onClick={decrementCounter} negative content="Decrement" />
     </div>
