c31463ea7c81a4901ca2014622ae1db59595f11c
Elias Norrby <elias.norrby@gmail.com>
2019-10-11 16:13:59 +0200

    L78: Creating a reusable Text area input

--- revents/src/features/events/EventForm/EventForm.jsx
+++ revents/src/features/events/EventForm/EventForm.jsx
@@ -7,6 +7,7 @@ import { Segment, Form, Button, Grid, Header } from "semantic-ui-react";
 
 import { createEvent, updateEvent } from "../eventActions";
 import TextInput from "../../../app/common/form/TextInput";
+import TextArea from "../../../app/common/form/TextArea";
 
 const mapStateToProps = (state, ownProps) => {
   const eventId = ownProps.match.params.id;
@@ -69,7 +70,8 @@ class EventForm extends Component {
               />
               <Field
                 name="description"
-                component={TextInput}
+                component={TextArea}
+                rows={3}
                 placeholder="Event Tell us about your event"
               />
               <Header sub color="teal" content="Event Location Details" />
