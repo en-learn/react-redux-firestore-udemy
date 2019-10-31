9814489e8abddc7374b64c56223096936212a340
Elias Norrby <elias.norrby@gmail.com>
2019-10-31 07:40:26 +0100

    L88: Setting up Places autocomplete

--- revents/src/features/testarea/TestComponent.jsx
+++ revents/src/features/testarea/TestComponent.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { incrementCounter, decrementCounter } from "./testActions";
 import { Button } from "semantic-ui-react";
+import TestPlaceInput from "./TestPlaceInput";
 
 const mapStateToProps = state => ({
   data: state.test.data,
@@ -20,6 +21,10 @@ const TestComponent = props => {
       <h3>The answer is: {data}</h3>
       <Button onClick={incrementCounter} positive content="Increment" />
       <Button onClick={decrementCounter} negative content="Decrement" />
+      <br />
+      <br />
+      <br />
+      <TestPlaceInput />
     </div>
   );
 };
