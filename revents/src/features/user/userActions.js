753632472eea22d087f851915e74d739846011ab
Elias Norrby <elias.norrby@gmail.com>
2020-01-03 14:05:00 +0100

    L156: Dealing with duplicate photos on the page

--- revents/src/features/user/userActions.js
+++ revents/src/features/user/userActions.js
@@ -4,6 +4,7 @@ import {
   asyncActionFinish,
   asyncActionError,
 } from "../async/asyncActions"
+import cuid from "cuid"
 
 export const updateProfile = user => async (
   dispatch,
@@ -25,12 +26,13 @@ export const uploadProfileImage = (file, fileName) => async (
   getState,
   { getFirebase, getFirestore },
 ) => {
+  const imageName = cuid()
   const firebase = getFirebase()
   const firestore = getFirestore()
   const user = firebase.auth().currentUser
   const path = `${user.uid}/user_images`
   const options = {
-    name: fileName,
+    name: imageName,
   }
 
   try {
@@ -58,7 +60,7 @@ export const uploadProfileImage = (file, fileName) => async (
         subcollections: [{ collection: "photos" }],
       },
       {
-        name: fileName,
+        name: imageName,
         url: downloadURL,
       },
     )
