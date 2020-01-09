aeae77897cbed82f6e7347ca05b105109f77fdb7
Elias Norrby <elias.norrby@gmail.com>
2020-01-09 15:40:04 +0100

    L202: Adding the reply form

--- revents/src/features/events/EventDetailed/EventDetailedChat.jsx
+++ revents/src/features/events/EventDetailed/EventDetailedChat.jsx
@@ -1,10 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 import { Segment, Comment, Header } from "semantic-ui-react"
 import EventDetailedChatForm from "./EventDetailedChatForm"
 import { Link } from "react-router-dom"
 import { formatDistance } from "date-fns"
 
 const EventDetailedChat = ({ addEventComment, eventId, eventChat }) => {
+  const [showReplyForm, setShowReplyForm] = useState(false)
+
+  const handleOpenReplyForm = () => {
+    setShowReplyForm(true)
+  }
+
+  const handleCloseReplyForm = () => {
+    setShowReplyForm(false)
+  }
+
   return (
     <>
       <Segment
@@ -32,7 +42,15 @@ const EventDetailedChat = ({ addEventComment, eventId, eventChat }) => {
                   </Comment.Metadata>
                   <Comment.Text>{comment.text}</Comment.Text>
                   <Comment.Actions>
-                    <Comment.Action>Reply</Comment.Action>
+                    <Comment.Action onClick={handleOpenReplyForm}>
+                      Reply
+                    </Comment.Action>
+                    {showReplyForm && (
+                      <EventDetailedChatForm
+                        addEventComment={addEventComment}
+                        eventId={eventId}
+                      />
+                    )}
                   </Comment.Actions>
                 </Comment.Content>
               </Comment>
