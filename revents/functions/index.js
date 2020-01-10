ad7a7c5efe11928f5c1e521b75c8b64c32c3d309
Elias Norrby <elias.norrby@gmail.com>
2020-01-10 09:07:22 +0100

    L211: Adding another cloud function

--- revents/functions/index.js
+++ revents/functions/index.js
@@ -3,25 +3,65 @@ const admin = require("firebase-admin")
 
 admin.initializeApp(functions.config().firebase)
 
+const newActivity = (type, event, id) => ({
+  type,
+  eventDate: event.date,
+  hostedBy: event.hostedBy,
+  title: event.title,
+  photoURL: event.hostPhotoURL,
+  timestamp: admin.firestore.FieldValue.serverTimestamp(),
+  hostUid: event.hostUid,
+  eventId: id,
+})
+
 exports.createActivity = functions.firestore
   .document("events/{eventId}")
   .onCreate(event => {
+    // The 'event' here is not a revents event,
+    // but the event triggering the function execution
     const newEvent = event.data()
 
     console.log(newEvent)
 
-    const activity = {
-      type: "newEvent",
-      eventDate: newEvent.date,
-      hostedBy: newEvent.hostedBy,
-      title: newEvent.title,
-      photoURL: newEvent.hostPhotoURL,
-      timestamp: admin.firestore.FieldValue.serverTimestamp(),
-      hostUid: newEvent.hostUid,
-      eventId: event.id,
-    }
+    const activity = newActivity("newEvent", newEvent, event.id)
 
     console.log(activity)
+
+    return admin
+      .firestore()
+      .collection("activity")
+      .add(activity)
+      .then(docRef => {
+        return console.log("Activity created with ID", docRef.id)
+      })
+      .catch(err => {
+        return console.log("Error adding activity", err)
+      })
+  })
+
+exports.cancelActivity = functions.firestore
+  .document("events/{eventId}")
+  .onUpdate((event, context) => {
+    let updatedEvent = event.after.data()
+    let previousEventData = event.before.data()
+    console.log({ event })
+    console.log({ context })
+    console.log({ updatedEvent })
+    console.log({ previousEventData })
+
+    if (
+      !updatedEvent.cancelled ||
+      updatedEvent.cancelled === previousEventData.cancelled
+    )
+      return false
+
+    const activity = newActivity(
+      "cancelledEvent",
+      updatedEvent,
+      context.params.eventId,
+    )
+
+    console.log({ activity })
 
     return admin
       .firestore()
